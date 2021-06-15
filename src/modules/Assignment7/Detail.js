a0ccee28b24a3ff38ef08cf626671b2b0f9567af
Garima Gupta <garima.gupta@appinventiv.com>
2021-06-15 19:05:43 +0530

    Commit 3 of the Assignment

--- src/modules/Assignment7/Detail.js
+++ src/modules/Assignment7/Detail.js
@@ -5,27 +5,38 @@ import "./index.css";
 function Detail(props) {
   var viewData;
   viewData = JSON.parse(localStorage.getItem("datas"));
-  const handleChange = () => {};
-
+  const [value, setValue]= useState({
+    cylin:viewData.cylinders,
+  })
+  const handleSubmit = (event) => {
+    const data= datas.event.target.value;
+    setValue({
+      ...value,
+     
+    })
+    var dat = JSON.stringify(value);
+    localStorage.setItem("datas", dat);
+  }
   return (
     <div className={"detailDiv"}>
+       <div className={"detailText"}>
+        <h2>Name: {viewData.name}</h2>
+        <h4>Miles Per Gallon: {viewData.miles_per_gallon}</h4>
+        <h4>Cylinders: {viewData.cylinders}</h4>
+        <h4>Displacment: {viewData.displacement}</h4>
+        <h4>HorsePower: {viewData.horsepower}</h4>
+        <h4>Weight in lbs: {viewData.weight_in_lbs}</h4>
+        <h4>Accelaration: {viewData.acceleration}</h4>
+        <h4>Year: {viewData.year}</h4>
+        <h4>Origin: {viewData.origin}</h4>
+      </div>
       <form>
         <div>
-          <input className={"search"} type="text" onChange={handleChange} />
+          <input className={"search"} type="text" name="cyl"  onSubmit={handleSubmit} />
           <button type="submit">Change Request</button>
         </div>
       </form>
-      <div className={"detailText"}>
-        <h2>{viewData.name}</h2>
-        <h4>{viewData.miles_per_gallon}</h4>
-        <h4>{viewData.cylinders}</h4>
-        <h4>{viewData.displacement}</h4>
-        <h4>{viewData.horsepower}</h4>
-        <h4>{viewData.weight_in_lbs}</h4>
-        <h4>{viewData.acceleration}</h4>
-        <h4>{viewData.year}</h4>
-        <h4>{viewData.origin}</h4>
-      </div>
+     
     </div>
   );
 }
