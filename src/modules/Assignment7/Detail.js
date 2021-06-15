1ea5496d0030e53346015780c60f15ca0d9703fa
Garima Gupta <garima.gupta@appinventiv.com>
2021-06-15 18:18:41 +0530

    Succesfully rendered the searched element in the second screen

--- src/modules/Assignment7/Detail.js
+++ src/modules/Assignment7/Detail.js
@@ -5,37 +5,27 @@ import "./index.css";
 function Detail(props) {
   var viewData;
   viewData = JSON.parse(localStorage.getItem("datas"));
-  const handleChange=() =>{
-    
-  }
+  const handleChange = () => {};
 
   return (
-    
     <div className={"detailDiv"}>
-         <form>
+      <form>
         <div>
-          <input
-            className={"search"}
-            type="text"
-            field="weight"
-            value={value.weight}
-            onChange={handleChange}
-          />
-          <button type="submit" >Change Request</button>
+          <input className={"search"} type="text" onChange={handleChange} />
+          <button type="submit">Change Request</button>
         </div>
       </form>
       <div className={"detailText"}>
-      <h2>{viewData.name}</h2>
-      <h4>{viewData.miles_per_gallon}</h4>
-      <h4>{viewData.cylinders}</h4>
-      <h4>{viewData.displacement}</h4>
-      <h4>{viewData.horsepower}</h4>
-      <h4>{viewData.weight_in_lbs}</h4>
-      <h4>{viewData.acceleration}</h4>
-      <h4>{viewData.year}</h4>
-      <h4>{viewData.origin}</h4>
+        <h2>{viewData.name}</h2>
+        <h4>{viewData.miles_per_gallon}</h4>
+        <h4>{viewData.cylinders}</h4>
+        <h4>{viewData.displacement}</h4>
+        <h4>{viewData.horsepower}</h4>
+        <h4>{viewData.weight_in_lbs}</h4>
+        <h4>{viewData.acceleration}</h4>
+        <h4>{viewData.year}</h4>
+        <h4>{viewData.origin}</h4>
       </div>
-   
     </div>
   );
 }
