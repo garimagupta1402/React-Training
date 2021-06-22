988f283c80b5cba83bc9b89a1a4019dfc5542185
Garima Gupta <garima.gupta@appinventiv.com>
2021-06-23 01:00:16 +0530

    Assignment 9 (Routing with React)

--- src/modules/Assignment9/product.js
+++ src/modules/Assignment9/product.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { useParams } from "react-router";
 import data from "./data";
-import './index.css';
+import "./index.css";
 
 function Product1() {
-  console.log(data[0], "product1");
+  const { id } = useParams();
+
   return (
     <>
       {data.map((product) => (
         <div className={"products"}>
-          <h1>{product.name}</h1>
-          <h3>{product.price}</h3>
+          
           <img src={product.image} />
+          <div className={"subDiv"}>
+          <h3>{product.id}</h3>
+          <h3 className={"name"}>{product.name}</h3>
+          </div>
+          <h2>{product.price}</h2>
+     
         </div>
       ))}
+    
     </>
   );
 }
